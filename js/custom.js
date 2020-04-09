b3cc9dc6de2cb8c305e62c6b72665664e3ef7fbe
Yousuf-Gazi <55175452+Yousuf-Gazi@users.noreply.github.com>
2020-04-10 01:48:02 +0600

    coming soon

---
+++ js/custom.js
@@ -0,0 +1,33 @@
+// typing effect start
+var typed = new Typed('.typing', {
+    strings: ["We are coming soon", "Stay with us!"],
+    loop: true,
+    typeSpeed: 40,
+    backSpeed: 40,
+    backDelay: 1000,
+});
+// typing effect end
+
+
+// countdown start
+$("#countdown")
+.countdown("2021/03/02", function(event) {
+
+    $('#days').text(
+        event.strftime('%D')
+    );
+
+    $('#hours').text(
+        event.strftime('%H')
+    );
+
+    $('#minutes').text(
+        event.strftime('%M')
+    );
+
+    $('#seconds').text(
+        event.strftime('%S')
+    );
+
+})
+// countdown end
